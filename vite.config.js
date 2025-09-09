baf2b4f40d4fbc22a10aff0f71010d297b3c3b88
leehongsoon <jasonlee1386@gmail.com>
2025-09-09 21:48:15 +0800

    Update vite.config.js with base URL

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
-  preview: {
-    port: 3000,
-  },
-});
+  base: 'leehongsoon1386',
+})
